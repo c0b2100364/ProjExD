530124c7918ecd809e69aa3407ce34014ddfda11
c0b21003 <c0b2100364@edu.teu.ac.jp>
2022-10-04 15:30:24 +0900

    練習7まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,8 +7,15 @@ def click_number(event):
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
 
+
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
+
 root =tk.Tk()
-root.geometry("300x500")
+root.geometry("300x570")
 
 entry = tk.Entry(root, width=10, font=(", 40"), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
@@ -25,5 +32,9 @@ for i, num in enumerate(numbers+operators, 1):
     if i%3 == 0:
         r += 1
         c = 0
+
+btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_equal)
+btn.grid(row=r, column=c)
 
 root.mainloop()
